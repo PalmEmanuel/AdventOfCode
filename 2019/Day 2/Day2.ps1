fbeca70e5b78ee4570c5c87c6458555464a17992
Emanuel Palm <ep__palm@hotmail.com>
2019-12-02 22:09:12 +0100

    Completed day 2

--- 2019/Day 2/Day2.ps1
+++ 2019/Day 2/Day2.ps1
@@ -1,23 +1,63 @@
 $PuzzleInput = (Get-Content .\input.txt) -split ',' | ForEach-Object { [int]$_ }
 
-$PuzzleInput[1] = 12
-$PuzzleInput[2] = 2
+function Get-IntcodeOutput {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [int[]]$PuzzleInput,
 
-$ProgramState = $PuzzleInput
+        [Parameter(Mandatory)]
+        [int]$Verb,
 
-:outerloop for ($i = 0; $i -lt $ProgramState.Count-1; $i += 4) {
-    "$i / $($ProgramState.Count)"
-    ""
-    switch ([int]$ProgramState[$i]) {
-        1 {
-            $ProgramState[$ProgramState[$i+3]] = $ProgramState[$ProgramState[$i+1]] + $ProgramState[$ProgramState[$i+2]]
+        [Parameter(Mandatory)]
+        [int]$Noun
+    )
+
+    $ProgramState = $PuzzleInput
+    
+    $PuzzleInput[1] = $Verb
+    $PuzzleInput[2] = $Noun
+
+    :outerloop for ($i = 0; $i -lt $ProgramState.Count - 1; $i += 4) {
+        switch ([int]$ProgramState[$i]) {
+            1 {
+                $ProgramState[$ProgramState[$i + 3]] = $ProgramState[$ProgramState[$i + 1]] + $ProgramState[$ProgramState[$i + 2]]
+            }
+            2 {
+                $ProgramState[$ProgramState[$i + 3]] = $ProgramState[$ProgramState[$i + 1]] * $ProgramState[$ProgramState[$i + 2]]
+            }
+            99 { break :outerloop }
+            Default { throw "Something went wrong, opcode is $i" }
         }
-        2 {
-            $ProgramState[$ProgramState[$i+3]] = $ProgramState[$ProgramState[$i+1]] * $ProgramState[$ProgramState[$i+2]]
+    }
+
+    return $ProgramState[0]
+}
+
+$Result = Get-IntcodeOutput -PuzzleInput $PuzzleInput -Verb 12 -Noun 2
+
+# 3409710
+"Part one: $Result"
+
+# Brute force it to find answers where noun x and verb y produces output 19690720
+:outerloop for ($i = 0; $i -lt 99; $i++) {
+    for ($j = 0; $j -lt 99; $j++) {
+        $TempResult = Get-IntcodeOutput -PuzzleInput $PuzzleInput -Verb $j -Noun $i
+
+        if ($TempResult -eq 19690720)
+        {
+            $FinalParams = @{
+                Verb = $i
+                Noun = $j
+            }
+
+            break :outerloop
         }
-        99 { break :outerloop }
-        Default { throw "Something went wrong, opcode is $i" }
     }
 }
 
-"Part one: $($ProgramState[0])"
+# Times 100
+$Result = 100 * $FinalParams.Noun + $FinalParams.Verb
+
+# 7912
+"Part two: $Result"
