4f736175eb9fabec363118290916cd02284dbe58
Emanuel Palm <emanuel+github@pipe.how>
2022-12-01 08:00:10 +0100

    day 1 2022

---
+++ 2022/2022/Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var input = File.ReadAllLines(".\\..\\..\\..\\input.txt");
+
+List<Elf> elfList = new List<Elf>();
+elfList.Add(new Elf());
+foreach (var line in input)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        elfList.Add(new Elf());
+    }
+    else
+    {
+        elfList[elfList.Count() - 1].Calories += int.Parse(line);
+    }
+}
+
+Console.WriteLine(elfList.OrderByDescending(elf => elf.Calories).First().Calories);
+
+Console.WriteLine(elfList.OrderByDescending(elf => elf.Calories).Take(3).Sum(elf => elf.Calories));
+
+class Elf
+{
+    public int Calories { get; set; } = 0;
+}
