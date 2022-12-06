6ef905353718bd2158a40be970cb9a1288606adf
Emanuel Palm <emanuel+github@pipe.how>
2022-12-06 15:37:10 +0100

    day6

---
+++ 2022/Day6/Program.cs
@@ -0,0 +1,45 @@
+﻿List<string> packetMarkerList = new List<string>();
+List<string> messageMarketList = new List<string>();
+bool startOfPacketFound = false;
+int counter = 0;
+using (var sr = new StreamReader(".\\..\\..\\..\\input.txt"))
+{
+    while (!sr.EndOfStream)
+    {
+        counter++;
+        var current = ((char)sr.Read()).ToString();
+
+        if (!startOfPacketFound) {
+            packetMarkerList.Add(current);
+
+            if (packetMarkerList.Count() == 4)
+            {
+                if (packetMarkerList.Distinct().Count() == 4)
+                {
+                    // Part 1
+                    Console.WriteLine(counter);
+                    startOfPacketFound = true;
+                }
+                else
+                {
+                    packetMarkerList.RemoveAt(0);
+                }
+            }
+        }
+
+        messageMarketList.Add(current);
+        if (messageMarketList.Count() == 14)
+        {
+            if (messageMarketList.Distinct().Count() == 14)
+            {
+                // Part 2
+                Console.WriteLine(counter);
+                break;
+            }
+            else
+            {
+                messageMarketList.RemoveAt(0);
+            }
+        }
+    }
+}
