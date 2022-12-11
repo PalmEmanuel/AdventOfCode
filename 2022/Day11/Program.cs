544f5f472f3899bda56902e0ff20a2e4f07f7885
Emanuel Palm <emanuel+github@pipe.how>
2022-12-11 23:10:15 +0100

    day 11

--- 2022/Day11/Program.cs
+++ 2022/Day11/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 
-using StreamReader sr = new StreamReader(".\\..\\..\\..\\test.txt");
-
+// Parse monkeys
+using StreamReader sr = new StreamReader(".\\..\\..\\..\\input.txt");
 while (!sr.EndOfStream)
 {
     string currentLine = sr.ReadLine();
@@ -13,7 +13,7 @@ while (!sr.EndOfStream)
         // Monkey id
         int id = int.Parse(currentLine.Split(' ').Last().TrimEnd(':'));
         // Monkey items
-        List<int> items = sr.ReadLine().Split(':').Last().Split(',').Select(s => int.Parse(s)).ToList();
+        List<long> items = sr.ReadLine().Split(':').Last().Split(',').Select(s => long.Parse(s)).ToList();
         // Monkey operation
         string operation = sr.ReadLine().Split(':').Last().Trim();
         // Monkey test
@@ -23,94 +23,110 @@ while (!sr.EndOfStream)
         // Monkey if test is false
         int falseMonkey = int.Parse(sr.ReadLine().Split(':').Last().Trim().Split(' ').Last());
 
-        Monkey.CreateMonkey(id, items, operation, test, trueMonkey, falseMonkey);
+        new Monkey(id, items, operation, test, trueMonkey, falseMonkey);
     }
 }
 
-for (int round = 0; round < 20; round++)
-{
-    for (int i = 0; i < Monkey.Monkeys.Count(); i++)
-    {
-        Monkey monkey = Monkey.Monkeys[i];
+// Get least common multiple to use with modulo
+var lcm = Monkey.Monkeys.Select(m => m.DivisibleBy).Aggregate((a, b) => a * b);
 
-        for (int j = 0; j < monkey.Items.Count(); j++)
+// Cause of the static list in the Monkey class, only run one of them
+//CalculateRounds(20, (i => i / 3));
+CalculateRounds(10000, (i => i % lcm));
+
+void CalculateRounds(int rounds, Func<long, long> reduce)
+{
+    for (int round = 0; round < rounds; round++)
+    {
+        for (int i = 0; i < Monkey.Monkeys.Count(); i++)
         {
-            monkey.InspectItem(j);
-            monkey.Items[j] /= 3;
-            monkey.TestAndThrowItem(j);
+            int monkeyItemCount = Monkey.Monkeys[i].Items.Count();
+            for (int j = 0; j < monkeyItemCount; j++)
+            {
+                // Since the first item is thrown, we will always inspect the first one
+                Monkey.Monkeys[i].InspectItem(0);
+
+                Monkey.Monkeys[i].Items[0] = reduce(Monkey.Monkeys[i].Items[0]);
+
+                Monkey.Monkeys[i].TestAndThrowItem(0);
+            }
         }
     }
+
+    var twoMostActiveMonkeys = Monkey.Monkeys.OrderByDescending(m => m.TimesInspected).Take(2);
+    Console.WriteLine(twoMostActiveMonkeys.First().TimesInspected * twoMostActiveMonkeys.Last().TimesInspected);
 }
 
-var twoMostActiveMonkeys = Monkey.Monkeys.OrderByDescending(m => m.TimesInspected).Take(2);
-Console.WriteLine(twoMostActiveMonkeys.First().TimesInspected * twoMostActiveMonkeys.Last().TimesInspected);
+void PrintMonkeys(int? round = null)
+{
+    if (round != null) { Console.WriteLine($"== After round {round} ==:"); }
+    for (int i = 0; i < Monkey.Monkeys.Count(); i++)
+    {
+        Console.WriteLine($"Monkey {Monkey.Monkeys[i].Id} inspected items {Monkey.Monkeys[i].TimesInspected} times.");
+    }
+}
 
 class Monkey
 {
     public static List<Monkey> Monkeys = new List<Monkey>();
 
     public int Id { get; set; }
-    public List<int> Items { get; set; }
-    public string TestString { get; set; }
+    public List<long> Items { get; set; }
     public int TrueMonkey { get; set; }
     public int FalseMonkey { get; set; }
-    public int TimesInspected { get; private set; }
+    public long TimesInspected { get; private set; }
 
-    public string OperationString { get; set; }
+    public int DivisibleBy { get; private set; }
+    public Func<long, long> Operation { get; set; }
 
-    public static Monkey CreateMonkey(int id, List<int> items, string operation, string test, int trueMonkey, int falseMonkey)
+    public Monkey(int id, List<long> items, string operation, string test, int trueMonkey, int falseMonkey)
     {
-        var newMonkey = new Monkey
-        {
-            Id = id,
-            Items = items,
-            OperationString = operation,
-            TestString = test,
-            TrueMonkey = trueMonkey,
-            FalseMonkey = falseMonkey,
-            TimesInspected = 0
-        };
-        Monkeys.Add(newMonkey);
-        return newMonkey;
+        Id = id;
+        Items = items;
+        TrueMonkey = trueMonkey;
+        FalseMonkey = falseMonkey;
+        TimesInspected = 0;
+
+        DivisibleBy = ParseTest(test);
+        Operation = ParseOperation(operation);
+        Monkeys.Add(this);
     }
 
     public void InspectItem(int index)
     {
-        Items[index] = UseOperation(Items[index]);
+        var item = Operation(Items[index]);
+        Items[index] = item;
+
         TimesInspected++;
     }
 
     public void TestAndThrowItem(int index)
     {
-        var testParts = Regex.Match(TestString, "divisible by (\\d+)$");
-        var divisibleBy = int.Parse(testParts.Groups[1].Value);
-
-        int toMonkeyIndex = Items[index] % divisibleBy == 0 ? TrueMonkey : FalseMonkey;
+        int toMonkeyIndex = Items[index] % DivisibleBy == 0 ? TrueMonkey : FalseMonkey;
 
         Monkeys[toMonkeyIndex].Items.Add(Items[index]);
         Items.RemoveAt(index);
     }
 
-    public int UseOperation(int old)
+    private int ParseTest(string testString)
     {
-        var opParts = Regex.Match(OperationString, "new = (\\w+) (.) (\\w+)$");
+        var testParts = Regex.Match(testString, "divisible by (\\d+)$");
+        return int.Parse(testParts.Groups[1].Value);
+    }
 
+    public Func<long, long> ParseOperation(string operationString)
+    {
+        var opParts = Regex.Match(operationString, "new = (\\w+) (.) (\\w+)$");
         var first = opParts.Groups[1].Value;
-        int firstValue = first == "old" ? old : int.Parse(first);
         var op = opParts.Groups[2].Value;
         var second = opParts.Groups[3].Value;
-        int secondValue = second == "old" ? old : int.Parse(second);
 
-        switch (op)
+        return (first, op, second) switch
         {
-            case "*":
-                return firstValue * secondValue;
-            case "-":
-                return firstValue - secondValue;
-            case "+":
-                return firstValue + secondValue;
-            default:
-                throw new Exception("Incorrect value!");
-        }
+            ("old", "*", "old") => old => old * old,
+            ("old", "*", var x) when long.TryParse(x, out var num) => old => old * num,
+            ("old", "+", var x) when long.TryParse(x, out var num) => old => old + num,
+            _ => throw new FormatException()
+        };
     }
 }
