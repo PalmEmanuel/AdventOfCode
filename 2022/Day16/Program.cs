f96033a9f1f48847abb98556c06b07428c4a618a
Emanuel Palm <emanuel+github@pipe.how>
2022-12-19 16:09:59 +0100

    day 16

--- 2022/Day16/Program.cs
+++ 2022/Day16/Program.cs
@@ -1,14 +1,15 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
 
 class Program
 {
     static async Task Main()
     {
         var valves = await ParseValves(".\\..\\..\\..\\input.txt");
-        var start = valves.First(v => v.Id == "AA");
-        var valvesWithPressure = valves.Where(v => v.FlowRate > 0).Select(v => (v.Id, v.FlowRate)).ToArray();
+        var start = valves.Values.First(v => v.Id == "AA");
+        var valvesWithPressure = valves.Values.Where(v => v.FlowRate > 0).Select(v => (v.Id, v.FlowRate)).ToArray();
         // Part 1
-        Console.WriteLine(GetPressure(30, valves.Where(v => v.FlowRate > 0), start));
+        Console.WriteLine(GetPressure(30, valves.Values.Where(v => v.FlowRate > 0), start));
         // Part 2
         Console.WriteLine(GetPressureWithElephant(new int[] { 26, 26 }, valvesWithPressure, new string[] { "AA", "AA" }, valves));
     }
@@ -36,124 +37,92 @@ class Program
         return pressure;
     }
 
-    static int GetPressureWithElephant(int[] minutesLeft, (string id, int flowRate)[] valvesWithPressure, string[] currentValves, List<Valve> valves)
+    static int GetPressureWithElephant(int[] minutesLeft, (string id, int flowRate)[] valvesWithPressure, string[] currentValves, Dictionary<string,Valve> valves)
     {
-        // Track minutes separately, like a turn-based thing
-        // Check if person already acted (by moving and opening, ie spending minutes)
-        // This enables alternating between the person and elephant
-        // Set actor to 0 or 1 based on relation between minutes left
+        int best = 0;
         int actor = minutesLeft[0] > minutesLeft[1] ? 0 : 1;
-        int pressure = 0;
-        var currentValve = valves.First(v => v.Id == currentValves[actor]);
-        foreach (var valve in valvesWithPressure)
+
+        var cur = valves[currentValves[actor]];
+        foreach (var t in valvesWithPressure)
         {
-            // Person is index 0, elephant is 1
-            // Get the minutes left of current actor, minus the distance to travel and the minute to open the valve
-            int currentMinutesLeft = minutesLeft[actor] - currentValve.DistanceTo[valve.id] - 1;
-            // If there's time left, try another valve
-            if (currentMinutesLeft > 0)
+            int newTimeToGo = minutesLeft[actor] - cur.DistanceTo[t.id] - 1;
+            if (newTimeToGo > 0)
             {
-                // Set up new minutes and valve pairs, the second element is the current one for the other actor
-                var newMinutesLeft = new int[] { currentMinutesLeft, minutesLeft[1 - actor] };
-                var newValves = new string[] { valve.id, currentValves[1 - actor] };
-                var potentialPressure = GetPressureWithElephant(newMinutesLeft, valvesWithPressure.Where(v => v.id != valve.id).ToArray(), newValves, valves);
-                int currentPressure = currentMinutesLeft * valve.flowRate + potentialPressure;
-                if (currentPressure > pressure)
-                {
-                    pressure = currentPressure;
-                }
+                var newTimes = new int[] { newTimeToGo, minutesLeft[1 - actor] };
+                var newLocs = new string[] { t.id, currentValves[1 - actor] };
+                int gain = newTimeToGo * t.flowRate + GetPressureWithElephant(newTimes, valvesWithPressure.Where(c => c.id != t.id).ToArray(), newLocs, valves);
+                if (best < gain) best = gain;
             }
         }
-
-        return pressure;
+        return best;
     }
 
-    static async Task<List<Valve>> ParseValves(string path)
+    static async Task<Dictionary<string,Valve>> ParseValves(string path)
     {
         var input = await File.ReadAllLinesAsync(path);
-        var valves = new List<Valve>();
+        var valves = new Dictionary<string, Valve>();
         var tunnelStrings = new List<IEnumerable<string>>();
         foreach (var line in input)
         {
             var lineParts = line.Split("; ");
             // Find id and flow rate
             var matches = Regex.Match(lineParts.First(), "^Valve ([A-Z]{2}) has flow rate=(\\d+)$").Groups.Values.Skip(1);
-            valves.Add(new Valve(matches.First().Value, int.Parse(matches.Last().Value)));
+            string id = matches.First().Value;
+            valves.Add(id, new Valve(matches.First().Value, int.Parse(matches.Last().Value)));
             // Add strings of tunnels
-            tunnelStrings.Add(lineParts.Last().Replace(",","").Split(' ')[4..]);
-        }
-        // All valves are created, add tunnel references
-        for (int i = 0; i < valves.Count(); i++)
-        {
-            foreach (var tunnel in tunnelStrings[i])
-            {
-                valves[i].TunnelsTo.Add(valves.First(v => v.Id == tunnel));
-            }
-            valves[i].TunnelsTo.OrderByDescending(t => t.FlowRate);
+            valves[id].TunnelsTo.AddRange(lineParts.Last().Replace(",", "").Split(' ')[4..]);
         }
 
         // A*-ish to set shortest distance to each valve
-        foreach (var fromValve in valves)
-        {
-            foreach (var toValve in valves.Where(v => v != fromValve))
-            {
-                var distance = FindValveDistance(fromValve, toValve);
-                fromValve.DistanceTo.Add(toValve.Id,distance);
-            }
-        }
+        SetShortestPath(valves);
 
         return valves;
     }
 
-    static int FindValveDistance(Valve fromValve, Valve toValve)
+    static void SetShortestPath(Dictionary<string, Valve> valves)
     {
-        List<Valve> explored = new();
-        List<Tuple<Valve, int>> valvesToExplore = new();
-        // Create a list of tuples to track the depth of each tunnel found
-        valvesToExplore.Add(new Tuple<Valve, int>(fromValve, 0));
+        foreach (var valve in valves.Values)
+        {
+            valve.DistanceTo[valve.Id] = 0;
+            SetValveDistance(valve, valve.Id, valves);
+        }
+    }
+
+    static void SetValveDistance(Valve currentValve, string toValve, Dictionary<string, Valve> valves)
+    {
+        HashSet<string> explored = new();
 
         // Until we've found the shortest path
-        while (valvesToExplore.Any())
+        while (currentValve != null && explored.Count < valves.Count)
         {
-            // Get the current valve to explore, and ordered by shortest relative depth
-            var currentValve = valvesToExplore.OrderBy(t => t.Item2).First();
-            var depth = currentValve.Item2;
+            explored.Add(currentValve.Id);
 
-            // If we found the valve
-            if (currentValve.Item1.Id == toValve.Id)
-            {
-                // Return relative depth of toValve from fromValve
-                return depth;
-            }
+            // Get the current valve to explore, and ordered by shortest relative distance
+            var distance = currentValve.DistanceTo[toValve] + 1;
 
             // If we didn't find the right valve, mark valve as explored
-            explored.Add(currentValve.Item1);
-            valvesToExplore.Remove(currentValve);
+            explored.Add(currentValve.Id);
 
             // For each tunnel from current valve
-            foreach (var possibleValve in currentValve.Item1.TunnelsTo)
+            foreach (var valveString in currentValve.TunnelsTo)
             {
-                // If we already explored it
-                if (explored.Any(v => v.Id == possibleValve.Id))
-                    continue;
-
-                if (valvesToExplore.Any(t => t.Item1.Id == possibleValve.Id))
+                // If we didn't already explore it
+                if (!explored.Contains(valveString))
                 {
-                    var potentialValve = valvesToExplore.First(t => t.Item1.Id == possibleValve.Id);
-                    if (potentialValve.Item2 < depth + 1)
+                    var tunnelValve = valves[valveString];
+
+                    if (tunnelValve.DistanceTo.ContainsKey(toValve))
                     {
-                        valvesToExplore.Remove(potentialValve);
-                        valvesToExplore.Add(new Tuple<Valve, int>(possibleValve, depth + 1));
+                        if (distance < tunnelValve.DistanceTo[toValve]) tunnelValve.DistanceTo[toValve] = distance;
+                    }
+                    else
+                    {
+                        tunnelValve.DistanceTo[toValve] = distance;
                     }
                 }
-                else
-                {
-                    valvesToExplore.Add(new Tuple<Valve, int>(possibleValve, depth + 1));
-                }
             }
+            currentValve = valves.Values.Where(v => !explored.Contains(v.Id) && v.DistanceTo.ContainsKey(toValve)).OrderBy(c => c.DistanceTo[toValve]).FirstOrDefault();
         }
-
-        throw new Exception("Could not find path between valves.");
     }
 }
 
@@ -161,13 +130,13 @@ class Valve
 {
     public string Id { get; }
     public int FlowRate { get; }
-    public List<Valve> TunnelsTo { get; }
+    public List<string> TunnelsTo { get; }
     public Dictionary<string, int> DistanceTo { get; }
 
     public Valve(string id, int flowRate)
     {
         DistanceTo = new Dictionary<string, int>();
-        TunnelsTo = new List<Valve>();
+        TunnelsTo = new List<string>();
         Id = id;
         FlowRate = flowRate;
     }
